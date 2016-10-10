903d739d7b60f8f45ac03033914d477a892bd3e0
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-10-10 19:13:36 -0400

    Actualizar vista site/about

--- views/site/about.php
+++ views/site/about.php
@@ -10,9 +10,11 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        Esta es la página Acerca de este sistema. Puede modificar el siguiente archivo para personalizar su contenido:
-    </p>
+    <br />
+    <p>El sistema de Expedientes es una aplicación que fue desarrollada <span class="glyphicon glyphicon-home" aria-hidden="true"></span> <b>Ninoska Corredor y Liz Garcia</b> como parte su proyecto socio-tecnológico del IUTEC.</p>
+    <p><span class="glyphicon glyphicon-link" aria-hidden="true"></span> Instagram: <a href="https://www.instagram.com/ninoskdcr/" title="Ninoska Corredor">@ninoskdcr</a> / <a href="https://www.instagram.com/liz_cgd/" title="Liz Garcia">@liz_cgd</a></p>
+    <p><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Correo electrónico: <a href="mailto:ninosk.dcr@gmail.com?subject=Sobre el sistema de Expedientes" title="Escríbeme">liz_carol_garcia_xo@hotmail.com</a> / <a href="mailto:liz_carol_garcia_xo@hotmail.com?subject=Sobre el sistema de Expedientes" title="Escríbeme">liz_carol_garcia_xo@hotmail.com</a></p>
+    <p><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> Teléfonos: 04269710021 / 041638868653</p>
+    <p><span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></span> Todos los derechos reservados 2016 - Ninoska Corredor y Liz Garcia.</p>
 
-    <code><?= __FILE__ ?></code>
 </div>
