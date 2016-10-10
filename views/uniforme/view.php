0a27638bac746c6e5cd24514b28278bf10d28a8d
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-10-10 19:07:10 -0400

    Agregar iconos glyphicon, Agregar funciones User::hasRole para comprar el role que tiene el usuario, en el modulo Uniforme

--- views/uniforme/view.php
+++ views/uniforme/view.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\bootstrap\Modal;
 use yii\widgets\DetailView;
+use webvimark\modules\UserManagement\models\User;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Uniforme */
@@ -16,24 +17,30 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Actualizar', ['update', 'id' => $model->cedula_id], ['class' => 'btn btn-primary']) ?>
         <?php
+        if (User::hasRole('ModificarRegistros')) {
+            echo Html::a('<span class="glyphicon glyphicon-edit"></span> ' . 'Actualizar', ['update', 'id' => $model->cedula_id], ['class' => 'btn btn-primary']);
+            echo '&nbsp;';
+        }
+
+        if (User::hasRole('EliminarRegistros')) {
             Modal::begin([
                 'header' => '<b>' . 'Eliminar Uniforme del Personal' . '</b>',
                 'footer' =>
-                    '<button type="button" class="btn btn-success" data-dismiss="modal">'.'No'.'</button>'
-                    .Html::a('Eliminar',
+                    '<button type="button" class="btn btn-success" data-dismiss="modal">' . '<span class="glyphicon glyphicon-remove"></span> ' . 'No' . '</button>'
+                    .Html::a('<span class="glyphicon glyphicon-trash"></span> ' . 'Eliminar',
                         ['delete', 'id' => $model->cedula_id],
                         [
                             'class' => 'btn btn-danger',
                             'data' => ['method' => 'post',],
                         ]
                     ),
-                'toggleButton' => ['label' => 'Eliminar', 'class' => 'btn btn-danger'],
+                'toggleButton' => ['label' => '<span class="glyphicon glyphicon-trash"></span> ' . 'Eliminar', 'class' => 'btn btn-danger'],
                 'size' => Modal::SIZE_SMALL
             ]);
             echo '¿Está seguro que desea eliminar este elemento?';
             Modal::end();
+        }
         ?>
     </p>
 
