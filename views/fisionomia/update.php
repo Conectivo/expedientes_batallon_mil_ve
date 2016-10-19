a5fc166cc96cdce5ccfbf86b0fe6c64db41caa71
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-10-19 11:28:50 -0400

    Remplazar referencia de cedula por nombre completo de la persona en el modulo Fisionomia

--- views/fisionomia/update.php
+++ views/fisionomia/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Fisionomia */
 
-$this->title = 'Actualizar Fisionomía del Personal: ' . $model->cedula_id;
+$this->title = 'Actualizar Fisionomía de ' . $model->cedula->nombres . " " .$model->cedula->apellidos;
 $this->params['breadcrumbs'][] = ['label' => 'Fisionomía del Personal', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->cedula_id, 'url' => ['view', 'id' => $model->cedula_id]];
+$this->params['breadcrumbs'][] = ['label' => $model->cedula->nombres . " " .$model->cedula->apellidos, 'url' => ['view', 'id' => $model->cedula_id]];
 $this->params['breadcrumbs'][] = 'Actualizar';
 ?>
 <div class="fisionomia-update">
